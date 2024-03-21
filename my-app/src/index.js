90098d0e952a8a3adf3de8bb7cb92fe507e8d5d6
Gabin Lee <gb94@ewhain.net>
2024-03-21 20:24:20 +0900

    [05] Components and Props-댓글 컴포넌트 만들기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,16 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Clock from './chapter_04/Clock';
+//import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
 
-setInterval(() => {
-  ReactDOM.render (
+ReactDOM.render (
   <React.StrictMode>
-    <Clock />
+    <CommentList />
   </React.StrictMode>,
   document.getElementById('root')
   );
-}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
