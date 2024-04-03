cd5c12ef36905275b3a02df2bfe0b521b3176056
Gabin Lee <gb94@ewhain.net>
2024-04-03 20:50:47 +0900

    [08] Handling Events-클릭 이벤트 처리하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,10 +8,11 @@ import reportWebVitals from './reportWebVitals';
 //import Clock from './chapter_04/Clock';
 //import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>,
   document.getElementById('root')
 )
