d900f9ecc706ab69a7bf1ecfd8af28e4dd95cb44
ch9968 <ichanhui578@gmail.com>
2024-04-05 17:28:14 +0900

    출석부 출력하기 실습

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <AttendanceBook />
   </React.StrictMode>,
   document.getElementById("root")
 );
