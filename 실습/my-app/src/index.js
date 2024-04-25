c0f41210d113409fdde4f155d610149d9c54e956
yoonjin-C <cyy000123@gmail.com>
2024-04-26 04:01:22 +0900

    (실습) 섭씨온도와 화씨온도 표시하기

--- 실습/my-app/src/index.js
+++ 실습/my-app/src/index.js
@@ -13,12 +13,13 @@ import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
 import SignUp from "./chpater_11/SignUp";
+import Calculator from "./chapter_12/Calculator";
 
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <Calculator />
   </React.StrictMode>
 );
 
