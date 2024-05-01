5ec3ba679dd11f0893b253f3825043f8dd9923d4
yoonjin-C <cyy000123@gmail.com>
2024-05-02 00:37:22 +0900

    (실습) Context를 사용하여 테마 변경 기능 만들기

--- 실습/my-app/src/index.js
+++ 실습/my-app/src/index.js
@@ -14,12 +14,14 @@ import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
 import SignUp from "./chpater_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
-
+import ProfileCard from "./chapter_13/ProfileCard";
 import { createRoot } from "react-dom/client";
+import DarkOrLight from "./chapter_14/DarkOrLight";
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <DarkOrLight />
   </React.StrictMode>
 );
 
